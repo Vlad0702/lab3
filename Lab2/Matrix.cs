0bab25bb5568462e9241d4ff2b8c24260e1c25d0
vlad07072002li@gmail.com <vlad07072002li@gmail.com>
2020-06-04 12:50:53 +0300

    Добавьте файлы проекта.

---
+++ Lab2/Matrix.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Matrix
+    {
+
+        public int Rows;
+        public int Columns;
+
+        public Matrix(int[] size, int[,] members)
+        {
+            Size = size;
+            Members = members;
+            Rows = Members.GetUpperBound(0) + 1;
+            Columns = Members.Length / Rows;
+            if (Columns != size[1] || Rows != size[0])
+                throw new Exception("Размерность матрицы не соответствует заданному");
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Console.Write($"{Members[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(); Console.WriteLine();
+        }
+
+        public bool IsUnit()
+        {
+            if (Rows != Columns)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Rows; i++)
+            {
+                if (Members[i, i] != 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static Matrix operator +(Matrix A, Matrix B)
+        {
+            if (A.Size[0] != B.Size[0] || A.Size[1] != B.Size[1])
+            {
+                throw new Exception("Размерности матриц не совпадают");
+            }
+
+            int[,] newMatrix = new int[A.Columns, A.Rows];
+
+            for (int i = 0; i < A.Rows; i++)
+            {
+                for (int j = 0; j < A.Columns; j++)
+                {
+                    newMatrix[i, j] = A.Members[i, j] + B.Members[i, j];
+                }
+            }
+
+            return new Matrix(A.Size, newMatrix);
+        }
+
+        public static Matrix operator -(Matrix A, Matrix B)
+        {
+            if (A.Columns != B.Columns || A.Rows != B.Rows)
+            {
+                throw new Exception("Размерности матриц не совпадают");
+            }
+
+            int[,] newMatrix = new int[A.Columns, A.Rows];
+
+            for (int i = 0; i < A.Rows; i++)
+            {
+                for (int j = 0; j < A.Columns; j++)
+                {
+                    newMatrix[i, j] = A.Members[i, j] - B.Members[i, j];
+                }
+            }
+
+            return new Matrix(A.Size, newMatrix);
+        }
+
+        public static Matrix operator *(Matrix A, Matrix B)
+        {
+            if (A.Columns != B.Rows)
+            {
+                throw new Exception("Размерности матриц не совпадают");
+            }
+
+            int[,] newMatrix = new int[A.Rows, B.Columns];
+
+            for (int i = 0; i < A.Rows; i++)
+            {
+                for (int j = 0; j < B.Columns; j++)
+                {
+                    for (int k = 0; k < A.Columns; k++)
+                    {
+                        newMatrix[i, j] += A.Members[i, k] * B.Members[k, j];
+                    }
+                }
+            }
+
+            return new Matrix(new int[] { A.Rows, B.Columns }, newMatrix);
+        }
+
+        public static Matrix operator *(Matrix A, int b)
+        {
+            return MultyByNum(A, b);
+        }
+
+        static Matrix MultyByNum(Matrix A, int b)
+        {
+            int[,] newMatrix = new int[A.Columns, A.Rows];
+
+            for (int i = 0; i < A.Rows; i++)
+            {
+                for (int j = 0; j < A.Columns; j++)
+                {
+                    newMatrix[i, j] = A.Members[i, j] * b;
+                }
+            }
+
+            return new Matrix(A.Size, newMatrix);
+        }
+
+        public static Matrix operator *(int a, Matrix B)
+        {
+            return MultyByNum(B, a);
+
+        }
+
+
+        public int[] Size { get; }
+        public int[,] Members { get; set; }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix A = new Matrix(new int[] { 3, 3 }, new int[,] { { 1, 2, 2 }, { 0, 3, 1 }, { 1, 0, 0 } });
+            Matrix B = new Matrix(new int[] { 3, 3 }, new int[,] { { 0, 0, 1 }, { -1/4, -1/2, 1/4 }, { 3/4, -1/2, -7/10 } });
+
+            Console.WriteLine("Матрица А:");
+            A.Print();
+            Console.WriteLine("Матрица B:");
+            B.Print();
+            Console.WriteLine($"Мацтрица А еденичная: {A.IsUnit().ToString()}");
+            Console.WriteLine($"Мацтрица B еденичная: {B.IsUnit().ToString()}");
+
+            Matrix D = 3 * A * B + (A - B) * A;
+
+            Console.WriteLine("\nМатрица D (3 * A * B + (A - B) * A):");
+            D.Print();
+            Console.WriteLine($"Мацтрица D еденичная: {D.IsUnit().ToString()}");
+
+            Console.ReadKey();
+        }
+    }
+}
